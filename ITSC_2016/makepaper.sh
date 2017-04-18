ab43d8ce30ba301a20a643e9ef5c40484989a203
trucomanx <fernando.pujaico.rivera@gmail.com>
2017-04-18 16:51:32 -0300

    Mejorando el articulo

---
+++ ITSC_2016/makepaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pdflatex -interaction=nonstopmode Artigo_ITSC_2016.tex
+pdflatex -interaction=nonstopmode Artigo_ITSC_2016.tex
+pdflatex -interaction=nonstopmode Artigo_ITSC_2016.tex
+
+./clean.sh
