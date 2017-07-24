55c7a5ffc8bb116783f361aab50c54de3f973519
Eduardo Afonso <eduardo.afonso@engautomacao.ufla.br>
2017-07-23 22:47:47 -0300

    versão português

---
+++ ITSC_2016/makepaper-port.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pdflatex -interaction=nonstopmode Artigo-versao-portugues.tex
+pdflatex -interaction=nonstopmode Artigo-versao-portugues.tex
+pdflatex -interaction=nonstopmode Artigo-versao-portugues.tex
+
+#./clean.sh
